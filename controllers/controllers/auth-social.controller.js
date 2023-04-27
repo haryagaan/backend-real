c4039f1e203f89a93b7f5f1f4a2f68be1c857975
Munkhtsog69696969 <95847767+Munkhtsog69696969@users.noreply.github.com>
2023-04-27 19:32:56 +0800

    Add files via upload

---
+++ controllers/controllers/auth-social.controller.js
@@ -0,0 +1,34 @@
+const { UserSocial } = require('../models/user-social.module');
+
+exports.SocialAuth = async (req, res) => {
+    const { displayName, email, socialUid, socialType, imageUrl } = req.body;
+
+    try {
+        const existingUser = await UserSocial.findOne({ socialUid: socialUid });
+
+        if (existingUser) {
+            res.status(200).json({ user: existingUser, isNew: false });
+        } else {
+            const spaceIndex = displayName.indexOf(' ');
+
+            const firstName = displayName.slice(0, spaceIndex);
+
+            const lastName = displayName.slice(spaceIndex + 1, displayName.length);
+
+            const newUser = await UserSocial.create({
+                firstName,
+                lastName,
+                email,
+                imageUrl,
+                socialType,
+                socialUid,
+            });
+
+            await newUser.save();
+
+            res.status(201).json({ user: newUser, isNew: true });
+        }
+    } catch (err) {
+        res.status(400).send(err);
+    }
+};
---
+++ controllers/controllers/jobPost.controller.js
@@ -0,0 +1,89 @@
+const {JobPost}=require("../models/jobPost.module");
+
+const {Job}=require("../models/job.module");
+
+const { JobCategory } = require("../models/jobCategory.module");
+
+const {User}=require("../models/user.module");
+
+const {UserSocial}=require("../models/user-social.module")
+
+exports.createPost=async(req,res)=>{
+    const {
+        title,
+        mainText,
+        price,
+    } =req.body;
+
+    const id=req.params.id;
+
+    const job=req.params.job;
+
+    const category=req.params.category;
+
+
+    let creatorType;
+
+    let creatorRating;
+
+    try{
+        if(!title || !mainText || !price || !id || !job || !category){
+            return res.status(400).send("Insufficient requests");
+        }
+
+        //user
+
+        const existingUser=await User.findById(id);
+
+        const existingUserSocial=await UserSocial.findById(id);
+
+        if(!existingUser && !existingUserSocial){
+            return res.status(404).send("No user found");
+        }else{
+            if(existingUser){
+                creatorRating=existingUser.rating;
+                creatorType="basic";
+            }else{
+                creatorType="social";
+            }
+        }
+
+        //job
+
+        const existingJob=await Job.findById(job);
+
+        if(!existingJob){
+            return res.status(404).send("Job not found");
+        }
+
+        //category
+
+        const existingCategory=await JobCategory.findById(category);
+
+        if(!existingCategory){
+            return res.status(404).send("Category not found");
+        }
+
+        const hourlyPrice=price+"MNT"+"/h";
+
+        const newPost=await JobPost.create({
+            title,
+            mainText,
+            price:hourlyPrice,
+            job,
+            category,
+            creatorId:id,
+            creatorIdSocial:id,
+            creatorType,
+            creatorRating
+        });
+
+        existingJob.posts.push(newPost._id);
+
+        await existingJob.save();
+
+        res.status(200).json(newPost);
+    }catch(err){
+        res.send(err);
+    }
+}
