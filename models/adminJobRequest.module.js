5edd993964be3a9a78c29c0e9ef497b946aa41fb
Munkhtsog69696969 <95847767+Munkhtsog69696969@users.noreply.github.com>
2023-04-27 16:53:34 +0800

    Add files via upload

---
+++ models/adminJobRequest.module.js
@@ -0,0 +1,39 @@
+const { Schema, Types, model } = require('mongoose');
+
+const adminJobRequestSchema = new Schema({
+
+    jobName:{
+        type:String,
+        required:true,
+    },
+
+    category:{
+        type:String,
+        required:true,
+    },
+
+    createdAt:{
+        type:Date,
+        default:new Date(),
+    },
+
+    creatorId:{
+        type:Schema.Types.ObjectId,
+        ref:"users",
+        // required:true,
+    },
+
+    creatorIdSocial:{
+        type:Schema.Types.ObjectId,
+        ref:"users-socials",
+        // required:true,
+    },
+
+    creatorType:{
+        type:String,
+        required:true,
+    }
+
+});
+
+exports.AdminJobRequest = model('admin-job-requests', adminJobRequestSchema);
